a3b69927e8eb3d83b58f188f74332651f1791a0d
Nilisha Nagar <111676856+nilishanagar@users.noreply.github.com>
2024-04-24 05:14:20 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId=144553
+let email="nilisha@google.com"
+var accountPassword="12345"
+accountCity="Jaipur" //allowed but not good
+let accountState;
+
+
+//accountId=2 //not allowed
+
+/* Prefer not to use var bacause of issue in block scope and functional scope */
+
+accountEmail="hc@hc.com"
+accountPassword="56666"
+accountCity="Benguluru"
+
+//to print single value on console
+console.log(accountId);
+
+//to print multiple  values together
+console.table([accountId,email,accountPassword,accountCity,accountState])
+
+
