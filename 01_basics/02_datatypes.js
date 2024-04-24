dcfff5d9503a7095e79ab5cd9d26aeac8ef0c4b1
Nilisha Nagar <111676856+nilishanagar@users.noreply.github.com>
2024-04-24 05:14:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,38 @@
+/* documentation :
+1) https://tc39.es/ecma262/
+2)https://developer.mozilla.org/en-US/docs/Web/JavaScript 
+*/
+
+
+"use strict"; // treat all Js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3
+    + 3)            //code readability should be high
+
+
+/*
+Commands in javascript are CASE SENSITIVE
+
+8 Data types in javascript:
+ ->String      =>" "
+ ->Number      => range 2 to power 53
+ ->Boolean     => true/false
+ ->BigInt
+ ->undefined   =>
+ ->null        => standalone value
+ ->Symbol      => for uniqueness
+
+ ->Object
+*/
+
+
+let name="nilisha"
+let age=23
+let isLoggedIn=false;
+let state=null;
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);      //object
+console.log(typeof "nilisha")  //string
