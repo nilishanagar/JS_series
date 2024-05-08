d1013e75691135574cd54062fe8134ea24b20a7b
Nilisha Nagar <111676856+nilishanagar@users.noreply.github.com>
2024-05-08 14:00:25 +0000

    reduce

---
+++ 05_iterations/06_reduce.js
@@ -0,0 +1,32 @@
+const mynums=[1,2,3,4,5]
+
+const myTotal=mynums.reduce(function(acc,currval){
+    console.log(`acc:${acc} and currval :${currval}`);
+    return acc+currval
+},0)
+
+console.log(myTotal);
+
+
+//above function as arrow function
+const total=mynums.reduce((acc,currval)=>acc+currval,0)
+console.log(total);
+
+const shoppingCart=[
+    {item:"js course",
+     price:2999
+    },
+    {item:"java course",
+    price:2334
+    },
+    {
+        item:"mobile course",
+        price:5000
+    },
+    {item:"Ds course",
+        price:10000
+    }
+]
+
+const pricepay=shoppingCart.reduce((acc,val)=>acc+val.price,0)
+console.log(pricepay);
